0c7a9fcb2c25f456073b84a4a62dc263650f85df
liorbiu <liork.cryptobiu@gmail.com>
2018-10-04 13:00:54 +0300

    Add scripts for execute experiment

--- MATRIX/build.sh
+++ MATRIX/build.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-#sudo apt-get update
-#sudo apt-get install build-essential -y
-#sudo apt-get install cmake libgmp3-dev yasm m4 python libz-dev libsodium-dev -y
-#
-## install mpir
-#cd ../../
-#wget 'http://mpir.org/mpir-3.0.0.tar.bz2'
-#tar xf mpir-3.0.0.tar.bz2
-#cd mpir-3.0.0
-#./configure --enable-cxx
-#make
-#make check
-#sudo make install
-#
+sudo apt-get update
+sudo apt-get install build-essential -y
+sudo apt-get install cmake libgmp3-dev yasm m4 python libz-dev libsodium-dev -y
+
+# install mpir
+cd ~
+wget 'http://mpir.org/mpir-3.0.0.tar.bz2'
+tar xf mpir-3.0.0.tar.bz2
+cd mpir-3.0.0
+./configure --enable-cxx
+make
+make check
+sudo make install
 
 # install ntl-11.1.0
 cd ~
@@ -24,16 +23,16 @@ cd ntl-11.1.0/src
 make -j 4
 
 ## install SPDZ-2
-#cd ~/SPDZ-2/
-#make
-#
-## install SPDZ-protocol
-#cd ~ && git clone https://github.com/cryptobiu/MPCHonestMajorityForSpdz.git
-#cd MPCHonestMajorityForSpdz
-#cmake . && make
+cd ~/SPDZ-2/
+make
+
+# install SPDZ-protocol
+cd ~ && git clone https://github.com/cryptobiu/MPCHonestMajorityForSpdz.git
+cd MPCHonestMajorityForSpdz
+cmake . && make
 
 # install spdz-extension
-#cd ~ && git clone https://github.com/cryptobiu/SPDZ-2-Extension-MpcHonestMajority.git
+cd ~ && git clone https://github.com/cryptobiu/SPDZ-2-Extension-MpcHonestMajority.git
 cd ~ && cd SPDZ-2-Extension-MpcHonestMajority
 cmake . && make
 
---
+++ MATRIX/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# read the arguments without party id
+
+argc=$#
+argv=($@)
+values=""
+
+for (( j=1; j<argc; j++ )); do
+    values+=`echo ${argv[j]}`
+    values+=' '
+done
+
+idx=${1}
+
+if [ "$argc" -eq "2" ]; then
+    ./Server.x ${values}
+else
+    sleep 5
+    ./Player-Online.x ${values} ${idx}
+fi
+
+
